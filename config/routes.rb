26d7e3c0ff9e13950bc1c7e57d88c4aeb4136511
Richard <richie1988@gmail.com>
2019-01-02 23:41:31 -0500

    added a post route for newMember

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#main'
   get '/login', to: 'pages#main'
   get '/newMember', to: 'users#new'
+  post '/newMember', to: 'users#new'
   get '/users', to: 'users#index'
   get '/users/edit', to: 'users#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
