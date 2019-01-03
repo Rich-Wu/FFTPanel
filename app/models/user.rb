8a7d30c8e6175bb456e215bdd817ea773b92f016
Richard <richie1988@gmail.com>
2019-01-03 12:07:08 -0500

    added some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  before_validation :downcase_email
+  validates :email, :presence => true, :uniqueness => true
 
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: 'Richard', last_name: 'Wu', email: 'example@example.com', password: '11111111', age: 30, education: 'PHD')
+User.create(first_name: 'Richard', last_name: 'Wu', email: 'example@example.com', password: '11111111', age: 30, education: 4)
+
+99.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.number(2),
+    email: Faker::Internet.unique.email,
+    password: (0...8).map { (rand(10)+48).chr }.join,
+    education: Faker::Number.between(1, 4)
+  )
+end
