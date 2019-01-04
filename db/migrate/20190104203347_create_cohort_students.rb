d80403011536636c62cb094fb2efaa7f414948fa
Richard <richie1988@gmail.com>
2019-01-04 15:34:36 -0500

    added a new relationship table

--- db/migrate/20190104203347_create_cohort_students.rb
+++ db/migrate/20190104203347_create_cohort_students.rb
@@ -1,8 +1,6 @@
 class CreateCohortStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :cohort_students do |t|
-      t.integer :cohort_id
-      t.integer :student_id
 
       t.timestamps
     end
