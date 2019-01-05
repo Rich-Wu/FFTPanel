88c690e8589d440cc9a163132842c544758d714a
Richard <richie1988@gmail.com>
2019-01-05 18:07:39 -0500

    removed hours column from cohorts (to be added to courses)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_162631) do
+ActiveRecord::Schema.define(version: 2019_01_05_180104) do
 
   create_table "cohorts", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
     t.date "end_date"
-    t.integer "hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
