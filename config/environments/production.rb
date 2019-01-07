8faabd91fd9f3aaaf72e8187689dbd6d2b5c978d
Richard <richie1988@gmail.com>
2019-01-06 22:43:32 -0500

    trying again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -98,6 +98,3 @@ if ENV['RACK_ENV']
 else
   set :database, {adapter: "sqlite3", database: "database.sqlite3"}
 end
-
-config.assets.compile = true
-config.assets.digest = true
