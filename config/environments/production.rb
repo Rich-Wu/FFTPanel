bc580b6c40235a994bc43a1f1fef11a8695ca297
Richard Wu <Richie1988@gmail.com>
2026-02-14 16:44:34 -0500

    remove sentry configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
-end
